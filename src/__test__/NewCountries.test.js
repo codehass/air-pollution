a80f80c5c5da199a82e948da414399b83c687129
codehass <hassan.elouardy06@gmail.com>
2023-02-10 01:52:46 +0100

    fix error

--- src/__test__/NewCountries.test.js
+++ src/__test__/NewCountries.test.js
@@ -3,7 +3,8 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import NewCountries from '../components/Countries/NewCountries';
-import store from '../redux/configureStore.js';
+// eslint-disable-next-line
+import store from '../redux/configureStore';
 
 it('should render the NewCountry component Correctly', () => {
   const newCountries = render(
