2a1177f24a4b0c0d721426f740f504a6d6d7123f
codehass <hassan.elouardy06@gmail.com>
2023-02-10 01:47:34 +0100

    fix error

--- src/__test__/NewCountries.test.js
+++ src/__test__/NewCountries.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import NewCountries from '../components/Countries/NewCountries';
-import store from '../redux/configureStore';
+import store from '../redux/configureStore.js';
 
 it('should render the NewCountry component Correctly', () => {
   const newCountries = render(
